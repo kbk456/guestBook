a600cea5c22e2b42cbfadf5e1e059b9d65634f85
bkkim <kimbokyun311@naver.com>
2021-12-14 17:16:36 +0900

    Initial commit

---
+++ src/main/java/com/example/guestbook/service/GuestbookServiceImpl.java
@@ -0,0 +1,67 @@
+package com.example.guestbook.service;
+
+import com.example.guestbook.dto.GuestbookDTO;
+import com.example.guestbook.dto.PageRequestDTO;
+import com.example.guestbook.dto.PageResultDTO;
+import com.example.guestbook.entity.Guestbook;
+import com.example.guestbook.repository.GuestbookRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class GuestbookServiceImpl implements GuestbookService {
+    private  final GuestbookRepository repository;
+
+    @Override
+    public Long register(GuestbookDTO dto) {
+        log.info("DTO---");
+        log.info(dto);
+        Guestbook entity = dtoToEntity(dto);
+        log.info(entity);
+
+        repository.save(entity);
+
+        return entity.getGno();
+    }
+
+    @Override
+    public GuestbookDTO read(Long gno) {
+        Optional<Guestbook> result = repository.findById(gno);
+        return result.isPresent() ? entityToDto(result.get()) : null;
+    }
+
+    @Override
+    public void remove(Long gno) {
+        repository.deleteById(gno);
+    }
+
+    @Override
+    public void modify(GuestbookDTO dto) {
+        Optional<Guestbook> result = repository.findById(dto.getGno());
+        if(result.isPresent()){
+            Guestbook entity = result.get();
+            entity.changeContent(dto.getContent());
+            entity.changeTitle(dto.getTitle());
+            repository.save(entity);
+        }
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO,Guestbook> getList(PageRequestDTO requestDTO){
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+        Page<Guestbook> result = repository.findAll(pageable);
+
+        Function<Guestbook,GuestbookDTO> fn = (entity ->entityToDto(entity));
+
+        return new PageResultDTO<>(result,fn);
+    }
+}
